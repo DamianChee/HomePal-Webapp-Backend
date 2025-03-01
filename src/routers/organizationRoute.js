b74d3a22870cd44dc476a92843843ae7ac8479ee
Damian Chee <silverousmechs@gmail.com>
2025-03-01 16:24:00 +0800

    Create organizationRoute.js

---
+++ src/routers/organizationRoute.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const {
+  createOrganization,
+  getOrganization,
+  getOrganizations,
+  getOrganizationsByName,
+  getOrganizationsByContactInfo,
+} = require("../controllers/organizationController");
+
+const router = express.Router();
+
+router.get("/", getOrganizations);
+router.post("/new", createOrganization);
+router.get("/id/:organizationId", getOrganization);
+router.get("/name/:name", getOrganizationsByName);
+router.get("/contact/:contact", getOrganizationsByContactInfo);
+
+module.exports = router;
