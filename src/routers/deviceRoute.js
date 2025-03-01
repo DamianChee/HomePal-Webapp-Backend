8631b28d8e422e9bb33fafa75c5cc4bced8e855f
Damian Chee <silverousmechs@gmail.com>
2025-03-01 14:11:03 +0800

    Update deviceRoute.js
    Slight typo mistake oops

--- src/routers/deviceRoute.js
+++ src/routers/deviceRoute.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.get("/", getDevices);
 router.post("/new", createDevice);
-router.get("id/:deviceId", getDevice);
+router.get("/id/:deviceId", getDevice);
 router.get("/status/:status", getDevicesByStatus);
 
 module.exports = router;
