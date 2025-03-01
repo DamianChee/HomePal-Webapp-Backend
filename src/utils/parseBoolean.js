5ea2b9d2767446b38123f6dab10996758dd297ec
Damian Chee <silverousmechs@gmail.com>
2025-03-01 15:03:30 +0800

    Create parseBoolean.js
    Added a robust boolean parser in case the project changes hands and the frontend ends up sending different types of booleans, strings, numbers, etc. to check with the backend

---
+++ src/utils/parseBoolean.js
@@ -0,0 +1,7 @@
+function parseBoolean(value) {
+  if (typeof value === "boolean") return value;
+  if (!value) return false;
+  return String(value).toLowerCase() === "true";
+}
+
+module.exports = formatDate;
