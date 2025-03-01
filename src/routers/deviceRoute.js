c3fcc8134784a058e26a2785412ab96611c13580
Damian Chee <silverousmechs@gmail.com>
2025-03-01 14:10:00 +0800

    Update deviceRoute.js

--- src/routers/deviceRoute.js
+++ src/routers/deviceRoute.js
@@ -4,12 +4,14 @@ const {
   createDevice,
   getDevice,
   getDevices,
+  getDevicesByStatus,
 } = require("../controllers/deviceController");
 
 const router = express.Router();
 
 router.get("/", getDevices);
 router.post("/new", createDevice);
-router.get("/:deviceId", getDevice);
+router.get("id/:deviceId", getDevice);
+router.get("/status/:status", getDevicesByStatus);
 
 module.exports = router;
